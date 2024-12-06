e30fe83233267bb53bdb32d375a825ed8e196100
jaklae <40382167+jaklae@users.noreply.github.com>
2024-12-06 16:46:28 +0100

    AOC Day 3 Part 1

---
+++ AOC/AOC_03/Aoc03.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace AOC_03
+{
+    class Aoc03
+    {
+        static void Main(string[] args)
+        {
+            string projectRoot = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
+            string filePath = Path.Combine(projectRoot, "input.txt");
+
+            var lines = File.ReadAllLines(filePath);
+
+            string mulPattern = @"mul\((\d+),(\d+)\)";
+            Regex regex = new Regex(mulPattern);
+
+            int[][] mulArray = new int[lines.Length][];
+            int totalSum = 0;
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                var matches = regex.Matches(lines[i]);
+                mulArray[i] = new int[matches.Count * 2]; 
+
+                int k = 0; 
+                foreach (Match match in matches)
+                {
+                    int num1 = int.Parse(match.Groups[1].Value);
+                    int num2 = int.Parse(match.Groups[2].Value);
+
+                    mulArray[i][k++] = num1;
+                    mulArray[i][k++] = num2;
+
+                    totalSum += num1 * num2;
+                }
+            }
+            Console.WriteLine($"Total Sum: {totalSum}");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # AOC
+
 Advent of coding [https://adventofcode.com/]
 <br>
 Day 1 ⭐️⭐️ <br>
 Day 2 ⭐️⭐️ <br>
-Day 3 <br>
+Day 3 ⭐️ <br>
 Day 4 <br>
 Day 5 <br>
 Day 6 <br>
-Day <br>
